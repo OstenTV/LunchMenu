a86fa4dca4bd769e9fbc543427ad6054e7fec51f
OstenTV <info@ostentv.dk>
2024-10-30 23:25:37 +0100

    Route everything to api and add some testing routes

--- api/api/routes/api.php
+++ api/api/routes/api.php
@@ -3,6 +3,14 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+/**
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+ */
+
+ Route::redirect('/status', '/up');
+
+ Route::get('/hello', function () {
+    return 'Hello, World!';
+});
