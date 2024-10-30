d66dd3db3bd2fd95507048bb619a8f9478a9c406
OstenTV <info@ostentv.dk>
2024-10-30 22:00:56 +0100

    Update deploy.ps1

---
+++ api/deploy.ps1
@@ -0,0 +1,9 @@
+$SrcDir =  "C:\Users\admin_tosi\Documents\GitHub\LunchMenu\api\api";
+$DstDir = "D:\VirtualSites\LunchAPI\v2\2.0.0"
+&robocopy `"$SrcDir`" `"$DstDir`" /MIR /XF `".*`"
+pushd $DstDir;
+
+&composer install
+&php artisan config:cache
+
+popd
