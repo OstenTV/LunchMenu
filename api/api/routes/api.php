ae09ce0e96785f68f6d040c8c33948c483ff999b
OstenTV <info@ostentv.dk>
2024-10-31 21:08:35 +0100

    Add some routes that I think we will need

--- api/api/routes/api.php
+++ api/api/routes/api.php
@@ -7,10 +7,34 @@ use Illuminate\Support\Facades\Route;
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
- */
+*/
 
- Route::redirect('/status', '/up');
+// Returns a list of available locations.
+Route::get('/lunch/locations', function () {
+    return;
+});
 
- Route::get('/hello', function () {
-    return 'Hello, World!';
+// Returns a list of year + weeks that are available for a location.
+Route::get('/lunch/selection/{location}', function (int $location) {
+    return;
+});
+
+// Returns today's menu.
+Route::get('/lunch/menu/{location}', function (int $location) {
+    return;
+});
+
+// Returns the weekmenu for a given location and year + week.
+Route::get('/lunch/menu/{location}/{year}/{week}', function (int $location, int $year, int $week) {
+    return;
+});
+
+// Returns the asset for a dish in today's menu.
+Route::get('/lunch/asset/{location}/{type}', function (int $location, int $type) {
+    return 'daymenu';
+});
+
+// Returns the asset for a dish in the weekmenu.
+Route::get('/lunch/asset/{location}/{year}/{week}/{day}/{type}', function (int $location, int $year, int $week, string $type) {
+    return 'weekmenu';
 });
